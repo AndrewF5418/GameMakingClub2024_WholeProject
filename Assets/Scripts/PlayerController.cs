01c4f4ef839581e5d3ff71393b5555bed7543e3e
AndrewF5418 <fang13612978925@outlook.com>
2024-03-13 22:36:25 +0800

    Update PlayerController.cs
    Now, we use rigidBody to control the position of the player instead of using transform.position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,25 +8,37 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     //animator
     public Animator anim;
+    //set rigidBody
+    private Rigidbody2D rb;
     // Start is called before the first frame update
+    
     void Start()
     {
         //anim = GetComponent(Animator);
         anim.SetBool("isMovingUpDown",false);
         anim.SetBool("isMovingLeft",false);
         anim.SetBool("isMovingRight",false); 
+        //set rigidBody
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveInput = new Vector3(0f,0f,0f);
+        // Vector3 moveInput = new Vector3(0f,0f,0f);
+        // moveInput.x = Input.GetAxisRaw("Horizontal");
+        // moveInput.y = Input.GetAxisRaw("Vertical");
+        // moveInput.Normalize();
+        //transform.position += moveInput * moveSpeed*Time.deltaTime;
+        //use rigidBody to adjust positon instead of transform.position
+        Vector2 moveInput = new Vector2(0f, 0f);
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
-        transform.position += moveInput * moveSpeed*Time.deltaTime;
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.deltaTime);
         // set bool for moving animation by if condition 
-        if(moveInput != Vector3.zero){
+        if(moveInput != Vector2.zero){
             if(moveInput.x>0 ||
             (moveInput.x>0 && Input.GetAxisRaw("Vertical")==1)||
             (moveInput.x>0 && Input.GetAxisRaw("Vertical")==-1)
@@ -52,6 +64,7 @@ public class PlayerController : MonoBehaviour
             anim.SetBool("isMovingLeft",false);
             anim.SetBool("isMovingRight",false); 
         }    
-
-}
+    }
+   
+    
 }
